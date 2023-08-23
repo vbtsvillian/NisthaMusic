fc84924ef9750116e89560160b4cf740d394fde4
vbtsvillian <141561303+vbtsvillian@users.noreply.github.com>
2023-08-23 07:30:25 +0530

    Update youtube.py

--- Nistha/Modules/cache/youtube.py
+++ Nistha/Modules/cache/youtube.py
@@ -19,12 +19,12 @@ def download(url: str) -> str:
     duration = round(info["duration"] / 60)
     if duration > DURATION_LIMIT:
         raise DurationLimitError(
-            f"🛑 ᴠɪᴅᴇᴏs ʟᴏɴɢᴇʀ ᴛʜᴀɴ {DURATION_LIMIT} ᴍɪɴᴜᴛᴇ(s) ᴀʀᴇ'ᴛ ᴀʟʟᴏᴡᴇᴅ, ᴛʜᴇ ᴘʀᴏᴠɪᴅᴇᴅ ɪs {duration} ᴍɪɴᴜᴛᴇ(s)",
+            f"🛑 𝑉𝐼𝐷𝐸𝑂𝑆 𝐿𝑂𝑁𝐺𝐸𝑅 𝑇𝐻𝐴𝑁 {DURATION_LIMIT} 𝑀𝐼𝑁𝑈𝑇𝐸(s) 𝐴𝑅𝐸'𝑇 𝐴𝐿𝐿𝑂𝑊 𝑇𝐻𝐸 𝑃𝑅𝑂𝑉𝐼𝐷𝐸𝐷 𝐼𝑆 {duration} 𝑀𝐼𝑁𝑈𝑇𝐸(s)",
         )
     try:
         ydl.download([url])
     except:
         raise DurationLimitError(
-            f"🛑 ᴠɪᴅᴇᴏs ʟᴏɴɢᴇʀ ᴛʜᴀɴ {DURATION_LIMIT} ᴍɪɴᴜᴛᴇ(s) ᴀʀᴇ'ᴛ ᴀʟʟᴏᴡᴇᴅ, ᴛʜᴇ ᴘʀᴏᴠɪᴅᴇᴅ ɪs {duration} ᴍɪɴᴜᴛᴇ(s)",
+            f"🛑 𝑉𝐼𝐷𝐸𝑂𝑆 𝐿𝑂𝑁𝐺𝐸𝑅 𝑇𝐻𝐴𝑁 {DURATION_LIMIT} 𝑀𝐼𝑁𝑈𝑇𝐸(s) 𝐴𝑅𝐸'𝑇 𝐴𝐿𝐿𝑂𝑊 𝑇𝐻𝐸 𝑃𝑅𝑂𝑉𝐼𝐷𝐸𝐷 𝐼𝑆 {duration} 𝑀𝐼𝑁𝑈𝑇𝐸(s)",
         )
     return path.join("downloads", f"{info['id']}.{info['ext']}")
